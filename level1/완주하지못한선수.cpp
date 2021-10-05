53afe997a422db7ab511f7c301347e605eb955ed
imjae <code@kakao.com>
2021-10-06 06:04:28 +0900

    First commit

---
+++ level1/완주하지못한선수.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    return answer;
+}
