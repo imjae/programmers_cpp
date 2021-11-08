791fbb095d9f9c6683154135ea59b13c2e7e6dfa
imjae <code@kakao.com>
2021-11-08 13:49:47 +0900

    vector로 풀려던 노력

--- level1/완주하지못한선수.cpp
+++ level1/완주하지못한선수.cpp
@@ -6,33 +6,63 @@
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion);
+bool FindString(vector<string> v, string targetStr);
 
 int main()
 {
     vector<string> participant;
-    participant.push_back("leo");
-    participant.push_back("kiki");
-    participant.push_back("eden");
+    participant.push_back("mislav");
+    participant.push_back("stanko");
+    participant.push_back("mislav");
+    participant.push_back("ana");
 
     vector<string> completion;
-    completion.push_back("eden");
-    completion.push_back("kiki");
+    completion.push_back("stanko");
+    completion.push_back("ana");
+    completion.push_back("mislav");
 
     solution(participant, completion);
 }
 
-string solution(vector<string> participant, vector<string> completion) {
+string solution(vector<string> participant, vector<string> completion)
+{
     string answer = "";
 
-    for_each(participant.begin(), participant.end(), [&](string& n){
-        cout << n << endl;        //output : 1 2 3 4
+    vector<string>::iterator it;
+
+    for (it = participant.begin(); it != participant.end(); )
+    {
+        if (FindString(completion, *it))
+        {
+            it = participant.erase(it);
+        }
+        else {
+            it++;
+        }
+    }
+
+    for_each(participant.begin(), participant.end(), [&](string &n)
+    {
+        cout << n << endl;        //output : 1 2 3 4 });
     });
 
     return answer;
 }
 
+bool FindString(vector<string> v, string targetStr)
+{
+    bool result;
 
-
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (targetStr.compare(v[i]) == 0)
+        {
+            cout << "찾았다! : " << targetStr << endl;
+            return true;
+        }
+    }
+    return false;
+}
 
 // 완주하지 못한 선수
 // 문제 설명
