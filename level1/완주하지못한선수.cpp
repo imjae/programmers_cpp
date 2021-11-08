3b198a70fe2808e8fb2b55c6becc723ef810db23
imjae <code@kakao.com>
2021-11-08 09:41:44 +0900

    C++ 환경 설정

--- level1/완주하지못한선수.cpp
+++ level1/완주하지못한선수.cpp
@@ -1,9 +1,26 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+class App
+{
+    public:
+        void run() {cout<<"run"<<endl;}
+};
+
+int main()
+{
+    App app;
+    app.run();
+    return 0;
+}
+
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
+
+    printf("¾È³ç");
+
     return answer;
 }
