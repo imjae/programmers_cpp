725e503d46c84e9848922ac32c7a59ecdb4564d9
imjae <code@kakao.com>
2022-01-25 06:14:16 +0900

    상화좌우:배열크기입력버전

--- thiscoding/chapter04구현/상하좌우.cpp
+++ thiscoding/chapter04구현/상하좌우.cpp
@@ -12,10 +12,12 @@ int main()
 {
     int n = 5;
 
-    int a[5][5];
 
     int x = 1, y = 1;
 
+    cin >> n;
+    int a[n][n];
+    cin.ignore();
     string str;
 
     getline(cin, str);
@@ -24,19 +26,19 @@ int main()
 
     for (int i = 0; i < vec.size(); i++)
     {
-        if (vec[i] == "L" && y != 1)
+        if (vec[i] == "L" && y > 1)
         {
             y -= 1;
         }
-        else if (vec[i] == "R" && y != 5)
+        else if (vec[i] == "R" && y < n)
         {
             y += 1;
         }
-        else if (vec[i] == "U" && x != 1)
+        else if (vec[i] == "U" && x > 1)
         {
             x -= 1;
         }
-        else if (vec[i] == "D" && x != 5)
+        else if (vec[i] == "D" && x < n)
         {
             x += 1;
         }
