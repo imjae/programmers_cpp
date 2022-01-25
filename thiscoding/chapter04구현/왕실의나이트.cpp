127bba095567309299b356e269403bea2bf093bc
imjae <code@kakao.com>
2022-01-25 10:18:24 +0900

    구현:왕실의나이트 무작정버전

---
+++ thiscoding/chapter04구현/왕실의나이트.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool check(int x, int y, int size);
+
+string inputData;
+
+int dx[] = {2, 2, -2, -2, 1, 1, -1, -1};
+int dy[] = {1, -1, 1, -1, 2, -2, 2, -2};
+
+int result = 0;
+
+
+int main()
+{
+    cin >> inputData;
+
+    int x = inputData[0] - 'a' + 1;
+    int y = inputData[1] - '1' + 1;
+
+    for(int i=0; i<8; i++)
+    {
+        int tmpX = x + dx[i];
+        int tmpY = y + dy[i];
+
+        if(check(tmpX, tmpY, 8)) result++;
+
+    }
+
+    cout << result << endl;
+    return 0;
+}
+
+bool check(int x, int y, int size)
+{
+    if(x < 1 || y < 1 || x >= size || y >= size)
+        return false;
+
+    return true;
+}
