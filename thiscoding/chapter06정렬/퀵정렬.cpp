56bf80cf3f90265c194c43b997df6ac36a130156
imjae <code@kakao.com>
2022-01-26 06:31:59 +0900

    정렬:퀵정렬 인덱스 버전

--- thiscoding/chapter06정렬/퀵정렬.cpp
+++ thiscoding/chapter06정렬/퀵정렬.cpp
@@ -7,89 +7,32 @@
 
 using namespace std;
 
-void qSort(vector<int> vec);
-void vecPrint(vector<int> vec);
+int n = 10;
+int arr[10] = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
 
-int main()
-{
-    vector<int> vec12 = {5, 7, 9, 0, 3, 1, 6, 2, 4, 8};
-    qSort(vec12);
-
-    cout << "#############최종##############" << endl;
-    vecPrint(vec12);
-
-    return 0;
+void quickSort(int* arr, int start, int end) {
+    if (start >= end) return; // 원소가 1개인 경우 종료
+    int pivot = start; // 피벗은 첫 번째 원소
+    int left = start + 1;
+    int right = end;
+    while (left <= right) {
+        // 피벗보다 큰 데이터를 찾을 때까지 반복
+        while (left <= end && arr[left] <= arr[pivot]) left++;
+        // 피벗보다 작은 데이터를 찾을 때까지 반복
+        while (right > start && arr[right] >= arr[pivot]) right--;
+        // 엇갈렸다면 작은 데이터와 피벗을 교체
+        if (left > right) swap(arr[pivot], arr[right]);
+        // 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+        else swap(arr[left], arr[right]);
+    }
+    // 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+    quickSort(arr, start, right - 1);
+    quickSort(arr, right + 1, end);
 }
 
-void qSort(vector<int> vec)
-{
-    int s = vec.size();
-
-    if (s == 1)
-        return;
-
-    if (s == 2)
-    {
-        if (vec[0] > vec[1])
-            swap(vec[0], vec[1]);
-        return;
+int main(void) {
+    quickSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
     }
-
-    int lIndex = 1, rIndex = s - 1;
-
-    while (true)
-    {
-        for (int i = lIndex; i < s; i++)
-        {
-            if (vec[0] < vec[i])
-            {
-                lIndex = i;
-                break;
-            }
-        }
-        for (int i = rIndex; i >= 0; i--)
-        {
-            if (vec[0] >= vec[i])
-            {
-                rIndex = i;
-                break;
-            }
-        }
-
-        if (lIndex > rIndex)
-        {
-            if (rIndex == 0)
-                break;
-            swap(vec[0], vec[rIndex]);
-
-            vecPrint(vec);
-            cout << endl;
-            break;
-        }
-        else
-            swap(vec[lIndex], vec[rIndex]);
-
-        vecPrint(vec);
-        cout << endl;
-    }
-
-    if(rIndex == 0) rIndex = 1;
-
-    vector<int> lVec(vec.begin(), vec.begin() + rIndex);
-    vector<int> rVec(vec.begin() + rIndex + 1, vec.end());
-
-    cout << "@@@ part별 최종 @@@" << endl;
-    vecPrint(lVec);
-    cout << " / ";
-    vecPrint(rVec);
-    cout << endl;
-
-    qSort(lVec);
-    qSort(rVec);
-}
-
-void vecPrint(vector<int> vec)
-{
-    for (int i = 0; i < vec.size(); i++)
-        cout << vec[i] << " ";
 }
