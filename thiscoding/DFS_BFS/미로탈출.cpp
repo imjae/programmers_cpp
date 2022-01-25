0e6ff54d47accf02a884d2fb7664acb21dfd90b4
imjae <code@kakao.com>
2022-01-25 16:33:12 +0900

    BFS:미로탈출:실패

---
+++ thiscoding/DFS_BFS/미로탈출.cpp
@@ -0,0 +1,73 @@
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int graph[201][201];
+
+// 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+int bfs(int x, int y)
+{
+    queue<pair<int, int>> q;
+    q.push({x, y});
+
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                continue;
+            if (graph[nx][ny] == 0)
+                continue;
+            if (graph[nx][ny] == 1)
+            {
+                graph[nx][ny] = graph[x][y] + 1;
+                q.push({nx, ny});
+            }
+        }
+    }
+
+    return graph[n - 1][m - 1];
+}
+
+int main()
+{
+    // N, M을 공백을 기준으로 구분하여 입력 받기
+    cin >> n >> m;
+    // 2차원 리스트의 맵 정보 입력 받기
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%1d", &graph[i][j]);
+        }
+    }
+    // BFS를 수행한 결과 출력
+    cout << bfs(0, 0) << '\n';
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << graph[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
