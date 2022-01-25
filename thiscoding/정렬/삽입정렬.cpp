1d4a3536a2f7d30a986f18b9d6e5587db49aa125
imjae <code@kakao.com>
2022-01-25 19:24:06 +0900

    정렬:삽입정렬

--- thiscoding/정렬/삽입정렬.cpp
+++ thiscoding/정렬/삽입정렬.cpp
@@ -7,10 +7,25 @@
 
 using namespace std;
 
+int n = 10;
 int arr[] = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
 
 int main()
 {
+    for (int i = 1; i < n; i++)
+    {
+        int v = arr[i];
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j] < arr[j - 1])
+                swap(arr[j], arr[j - 1]);
+            else
+                break;
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+        cout << arr[i] << " ";
 
     return 0;
 }
