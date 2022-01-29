bcfbf420ed397dbf2f6e119e8a3ed145d282fcb0
imjae <code@kakao.com>
2022-01-30 07:38:59 +0900

    기출:그리디:만들수없는금액 두번째 복습 올립니다.

--- thiscoding/유형별기출문제/11_그리디/만들수없는금액RERE.cpp
+++ thiscoding/유형별기출문제/11_그리디/만들수없는금액RERE.cpp
@@ -13,7 +13,26 @@ vector<int> arr;
 int main()
 {
     cin >> n;
-    
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int target = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] > target) break;
+        
+        target += arr[i];
+    }
+
+    cout << target << endl;
 
     return 0;
 }
