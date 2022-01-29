1495645acd92760e787cd7afeda1eb68f2e3fca5
imjae <code@kakao.com>
2022-01-29 11:23:17 +0900

    기출:그리디:곱하기혹은더하기(1)

--- thiscoding/유형별기출문제/11_그리디/곱하기혹은더하기.cpp
+++ thiscoding/유형별기출문제/11_그리디/곱하기혹은더하기.cpp
@@ -14,17 +14,28 @@ int main()
 {
     cin >> s;
 
-    result = (int)s[0] - 48;
+    // result = (int)s[0] - 48;
+    long long result = s[0] - '0';
 
     cout << result << endl;
 
-    for (int i = 0; i < s.size() - 1; i++)
+    // 두 숫자가 1, 0, 음수일 떄는 더하는게 더 큰 값을 결과로 갖는다.
+
+    for (int i = 0; i < s.size(); i++)
     {
-        if (result == 0 || s[i + 1] == '0')
-            result += (int)s[i + 1] - 48;
+        int num = s[i] - '0';
+        if (num <= 1 || result <= 1)
+            result += num;
         else
-            result *= (int)s[i + 1] - 48;
+            result *= num;
     }
+    // for (int i = 0; i < s.size() - 1; i++)
+    // {
+    //     if (result == 0 || s[i + 1] == '0')
+    //         result += (int)s[i + 1] - 48;
+    //     else
+    //         result *= (int)s[i + 1] - 48;
+    // }
 
     cout << result << endl;
 
